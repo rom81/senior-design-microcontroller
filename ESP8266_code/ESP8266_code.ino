05eba85c36f24c737cb21c61d831c8115e5170f1
Rachel Misbin <rachelmisbin@pitt.edu>
2019-03-05 18:01:14 -0500

    Added infrastructure for connecting to MQTT client. Currently successfully connects to mqtt client

--- ESP8266_code/ESP8266_code.ino
+++ ESP8266_code/ESP8266_code.ino
@@ -5,14 +5,18 @@
 
 
 // Network config
-const char* network_name = "network_name";
-const char* password = "pass";
+//const char* ssid = "WIRELESS-PITTNET";
+//const char* password = "pass";
+const char* ssid = "WIRELESS-PITTNET";
+const char* security_type = "WPA-Enterprise";
+const char* encryption_type = "TKIP";
+const char* auth_method = "PEAP";
+const char* username = "rom81";
+const char* password = "";
 
 // MQTT server config
-const char* mqttServer = "mqtt.server.com";
+const char* mqttServer = "54.158.107.32";
 const int mqttPort = 1883;
-const char* mqttUser = "user";
-const char* mqttPassword = "password";
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
@@ -24,22 +28,22 @@ void setup()
   Serial.begin(115200);
   LightSensor.begin();
  
-//  WiFi.begin(network_name, password);
-//  Serial.println("about to connect");
-//
-//  while (WiFi.status() != WL_CONNECTED) 
-//  {
-//    delay(500);
-//    Serial.println("Connecting to WiFi..");
-//  }
-//  
-//  Serial.println("Connected to the WiFi network");
+  WiFi.begin(ssid, password);
+  Serial.println("about to connect");
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  
+  Serial.println("Connected to the WiFi network");
 }
 
 void loop()
 {
   uint16_t lux = LightSensor.GetLightIntensity();
-  Serial.print("Light: ");
-  Serial.println(lux);
+  Serial.print(lux);
+  Serial.println(" lux");
   delay(250);
 }
